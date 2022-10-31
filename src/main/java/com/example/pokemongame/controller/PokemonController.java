d5e04cd661113b1d2af8ace970c8ba3ea48411d4
cricsudheer <cricsudheer@gmail.com>
2022-10-31 13:26:00 +0530

    Created model for pokemon

--- src/main/java/com/example/pokemongame/controller/PokemonController.java
+++ src/main/java/com/example/pokemongame/controller/PokemonController.java
@@ -4,12 +4,14 @@ import com.example.pokemongame.repository.Pokemonrepository;
 import com.example.pokemongame.service.PokemonService;
 import models.GeneralResponse;
 import models.Pokemon;
+import models.PokemonResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class PokemonController {
@@ -37,13 +39,21 @@ public class PokemonController {
     public GeneralResponse addallPokemon(@RequestBody List<Pokemon> pokemons){
          int countOfPokemon =0;
          GeneralResponse generalResponse = new GeneralResponse();
-
+         generalResponse.setStatus(true);
          for(Pokemon pokemon : pokemons){
              countOfPokemon+=1 ;
              pokemonrepository.save(pokemon);
          }
          generalResponse.setMessage(String.format("Added a total of %d pokemons", countOfPokemon));
          return generalResponse ;
+    }
+
+    @GetMapping(value = "/search/{pokemonName}" , consumes =  "application/json" , produces = "application/json")
+    public PokemonResponse searchPokemon(@PathVariable String pokemonName){
+          PokemonResponse pokemonResponse = new PokemonResponse();
+          pokemonResponse.setPokemonList(pokemonrepository.findByName(pokemonName));
+          pokemonResponse.setMessage(String.format("Found a total of %d pokemons", pokemonResponse.getPokemonList().size()));
+          return pokemonResponse  ;
     }
 
 }
